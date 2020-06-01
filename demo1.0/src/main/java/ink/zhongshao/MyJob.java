5a626649cd93bd619140f9b7031e15bfe70be856
zs <zs@192.168.12.106>
2020-06-01 18:40:30 +0800

    线程事例

---
+++ demo1.0/src/main/java/ink/zhongshao/MyJob.java
@@ -0,0 +1,29 @@
+package ink.zhongshao;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
+public class MyJob implements Callable<String> {
+
+	private String jobName;
+
+	public MyJob(String jobName) {
+		super();
+		this.jobName = jobName;
+	}
+
+	@Override
+	public String call() throws Exception {
+
+		System.out.println("Ö´ÐÐÈÎÎñ:" + jobName);
+
+		if (jobName.equals("MyJob3")) {
+			TimeUnit.SECONDS.sleep(5);
+		} else {
+			TimeUnit.SECONDS.sleep(2);
+		}
+
+		return jobName;
+	}
+
+}
--- demo1.0/src/main/java/ink/zhongshao/Test.java
+++ demo1.0/src/main/java/ink/zhongshao/Test.java
@@ -1,9 +1,41 @@
 package ink.zhongshao;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("0000000000000==");
+
+		List<FutureTask<String>> tasks = new ArrayList<>();
+
+		for (int i = 0; i < 10; i++) {
+			MyJob myj = new MyJob("MyJob" + i);
+			FutureTask<String> task = new FutureTask<>(myj);
+			tasks.add(task);
+
+			Thread t = new Thread(task);
+			t.start();
+		}
+
+		tasks.stream().forEach((t) -> {
+			try {
+
+				String result = t.get();
+
+				if (result != null) {
+					System.out.println(result);
+				}
+			} catch (InterruptedException e) {
+
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+
+				e.printStackTrace();
+			}
+		});
 
 	}
 
