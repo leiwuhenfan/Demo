f40831f99a49d051d0e989960f3105b45f529419
zs <zs@zs-PC>
2020-08-26 14:34:16 +0800

    正则表达式测试demo

--- demo1.0/src/main/java/ink/zhongshao/Test.java
+++ demo1.0/src/main/java/ink/zhongshao/Test.java
@@ -1,104 +1,64 @@
 package ink.zhongshao;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Random;
-import java.util.Set;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+/**
+ * 正则验证
+ * 
+ * @author zs
+ * @date 2020年8月26日
+ */
 public class Test {
 
-	
-	private static Random random = ThreadLocalRandom.current();
-	
-	
+	// 大小写字母 数字 下划线 括号 长度 30
+	private String regex = "^[a-zA-Z\\d_()\\u4e00-\\u9fa5]{1,30}$";
+	private Pattern pattern = Pattern.compile(regex);
+
+	void math() {
+		Matcher m = pattern.matcher("喊aZAz901()_");
+		boolean result = m.matches();
+
+		// System.out.println("-----" + result);
+	}
+
+	void math2() {
+		String regex = "^[a-zA-Z\\d_()\\u4e00-\\u9fa5]{1,30}$";
+
+		// 需要对regex进行null判断
+		Pattern pattern = Pattern.compile(regex); // 验证不为null
+		Matcher m = pattern.matcher("喊aZAz901()_");
+		boolean result = m.matches();
+
+		// System.out.println("-----" + result);
+	}
+
 	public static void main(String[] args) {
-		System.out.println("0000000000000==");
 
-		// 替换
-		String kl = "\\";
+		Test test = new Test();
+		
+		/**
+		 * 
+		 * 【强制】在使用正则表达式时，利用好其预编译功能，可以有效加快正则匹配速度。
+		 *      说明：不要在方法体内定义：Pattern pattern = Pattern.compile(“规则”);
+		 * 
+		 */
+		Long start1 = System.currentTimeMillis();
+		for (int i = 0; i < 12000000; i++) {
+			test.math();//使用compile预编译提升性能
+		}
+		Long end1 = System.currentTimeMillis();
+		System.out.println("  pattern类变量的耗时: =   " + (end1 - start1));
 
-		// 斜杠替换，4个\替换(模糊查询使用like ? escape '/' %name%)
-		// 前台传入\
-		// 后台替换kl.replaceAll("\\\\", "、/\\\\");
-		kl = kl.replaceAll("%", "/%");
-		kl = kl.replaceAll("_", "/_");
-		kl = kl.replaceAll("/", "//");
-		kl = kl.replaceAll("\\\\", "/\\\\");
-
-		System.out.println(kl);
-
-		// set添加返回值
-		Set<String> acloSets = new HashSet<>();
-
-		System.out.println("acloSets.add(\"1\")=" + acloSets.add("1"));
-		System.out.println("acloSets.add(\"1\")" + acloSets.add("1"));
-		System.out.println("acloSets.add(\"2\")" + acloSets.add("2"));
-
-		System.out.println("nextInt(10)="+random.nextInt(10));
 		
 		
-		String[]  jk= acloSets.toArray(new String[0]);
-		System.out.println(jk);
-		
-		
-		Test t = new Test();
-		
-		t.TestArraytoList();
-		t.testListtoArray();
-		
+		Long start2 = System.currentTimeMillis();
+		for (int i = 0; i < 12000000; i++) {
+			test.math2();
+		}
+		Long end2 = System.currentTimeMillis();
+		System.out.println("  pattern局部变量的耗时: =   " + (end2 - start2));
+
 	}
-	
-	
-	void TestArraytoList() {
-		/*
-		【强制】使用工具类 Arrays.asList()把数组转换成集合时，不能使用其修改集合相关的方法，它的 add/remove/clear 方法会抛出 UnsupportedOperationException 异常。
-		说明：asList 的返回对象是一个 Arrays 内部类，并没有实现集合的修改方法。Arrays.asList 体现的是适配器模式，只是转换接口，后台的数据仍是数组。
-		 String[] str = new String[] { "chen", "yang", "hao" };
-		 List list = Arrays.asList(str);
-		第一种情况：list.add("yangguanbao"); 运行时异常。
-		第二种情况：str[0] = "change"; 也会随之修改，反之亦然。
-		*/
-		
-		String []dd = {"11","22","33"};
-		
-		List<String> lk = Arrays.asList(dd);
-		
-		dd[0]="11-gh";
-		
-		System.out.println(String.join(",", lk));
-		
-	}
-	
-	void testListtoArray() {
-		
-		/*
-		【强制】使用集合转数组的方法，必须使用集合的 toArray(T[] array)，传入的是类型完全一致、长度为 0 的空数组。
-		反例：直接使用 toArray 无参方法存在问题，此方法返回值只能是 Object[]类，若强转其它类型数组将出现
-		ClassCastException 错误。
-		正例：
-		List<String> list = new ArrayList<>(2);
-		list.add("guan");
-		list.add("bao");
-		String[] array = list.toArray(new String[0]);
-		 说明：使用 toArray 带参方法，数组空间大小的 length： 
-		1） 等于 0，动态创建与 size 相同的数组，性能最好。
-		2） 大于 0 但小于 size，重新创建大小等于 size 的数组，增加 GC 负担。
-		3） 等于 size，在高并发情况下，数组创建完成之后，size 正在变大的情况下，负面影响与 2 相同。
-		4） 大于 size，空间浪费，且在 size 处插入 null 值，存在 NPE 隐患。
-		*/
-		// set添加返回值
-		Set<String> acloSets = new HashSet<>();
-
-		System.out.println("acloSets.add(\"1\")=" + acloSets.add("1"));
-		System.out.println("acloSets.add(\"1\")" + acloSets.add("1"));
-		System.out.println("acloSets.add(\"2\")" + acloSets.add("2"));
-		String[]  jk= acloSets.toArray(new String[0]);
-		System.out.println(String.join(",", jk));
-		
-		
-	}
-	
 
 }
